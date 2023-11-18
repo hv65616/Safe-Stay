f18cc545ebaf0cc444c2f61163cc405b079d8495
Himanshu <hv65616@gmail.com>
2023-11-18 15:03:48 +0530

    user model created

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose, { model } from "mongoose";
+const userschema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const user = mongoose.model("User", userschema);
+export default user;
