26d939e796e234db6c40a068218d490a79d38b3f
Himanshu <hv65616@gmail.com>
2023-11-21 12:02:39 +0530

    error handle middleware

--- api/index.js
+++ api/index.js
@@ -15,7 +15,15 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use("/api/user", userrouter);
-app.use("/api/auth",authrouter)
+app.use("/api/auth", authrouter);
+app.use((err, req, res, next) => {
+  const statuscode = err.statuscode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statuscode).json({
+    success: false,
+    message,
+  });
+});
 app.listen(3000, (req, res) => {
   console.log("Server is running port 3000");
 });
