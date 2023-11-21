065c2bf6f01f2be1c2e1b20d9f476521356548ba
Aastha Agarwal <astha1916@gmail.com>
2023-11-21 12:10:30 +0530

    minor corrections

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,15 @@
 import {Link} from 'react-router-dom'
 
-function SignUp() {
+function SignUp() { 
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>
+    <div className='p-3 max-w-lg mt-16 mx-auto '>
+      <h1 className='text-3xl text-center  font-semibold my-7'>
        Sign Up 
        </h1>
-      <form className='flex flex-col gap-4 '>
-        <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username'></input>
-        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email'></input>
-        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password'></input>
+      <form className='flex flex-col gap-4'>
+        <input type='text' placeholder='Username' className='border p-3 rounded-lg' id='username' ></input>
+        <input type='email' placeholder='Email' className='border p-3 rounded-lg' id='email' ></input>
+        <input type='password' placeholder='Password' className='border p-3 rounded-lg' id='password'></input>
         <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 '>Sign up</button>
       </form>
       <div className='flex gap-2 mt-5'>
