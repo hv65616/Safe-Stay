842cb955fea6f462a2442d453641c3d1efd2a978
Himanshu <hv65616@gmail.com>
2023-11-21 10:47:10 +0530

    signup route include

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userrouter from "./routes/user.route.js";
+import authrouter from "./routes/auth.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGOURI)
@@ -12,7 +13,9 @@ mongoose
     console.log(err);
   });
 const app = express();
+app.use(express.json());
 app.use("/api/user", userrouter);
+app.use("/api/auth",authrouter)
 app.listen(3000, (req, res) => {
   console.log("Server is running port 3000");
 });
