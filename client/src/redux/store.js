f32420dfd4b2df3d805902815aa68c959923e35a
Aastha Agarwal <astha1916@gmail.com>
2023-11-23 16:30:46 +0530

    Add Redux Toolkit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { userReducer } from './user/userSlice'
+import userReducer from './user/userSlice'
 export const store = configureStore({
-  reducer: {
-    user : userReducer
-    },
+  reducer: {user : userReducer},
   middleware: (getDefaultMiddleware) =>
   getDefaultMiddleware({
     serializableCheck : false,
   }),
-})
+});
 
