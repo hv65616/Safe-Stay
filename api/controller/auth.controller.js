ef30e5bcd0e7614bf2cd22f20bb8f81235a3b7c4
Himanshu <hv65616@gmail.com>
2023-11-26 20:58:22 +0530

    google signin data save

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -29,7 +29,7 @@ export const signin = async (req, res, next) => {
       return next(errorhandler(401, "Invalid password"));
     }
     const token = jwt.sign({ id: validuser._id }, process.env.JWTSECRET);
-    const {password:pass,...rest} = validuser._doc;
+    const { password: pass, ...rest } = validuser._doc;
     res
       .cookie("access-token", token, {
         httpOnly: true,
@@ -37,6 +37,46 @@ export const signin = async (req, res, next) => {
       })
       .status(200)
       .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const google = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWTSECRET);
+      const { password: pass, ...rest } = user._doc;
+      res
+        .cookie("access-token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(rest);
+    } else {
+      const generatedpassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcrypt.hashSync(generatedpassword, 10);
+      const newuser = new User({
+        username:
+          req.body.name.split(' ').join('').toLowerCase() +
+          Math.random().toString(36).slice(-8),
+        email: req.body.email,
+        password: hashedPassword,
+        avatar: req.body.photoURL,
+      });
+      await newuser.save();
+      const token = jwt.sign({ id: newuser._id }, process.env.JWTSECRET);
+      const { password: pass, ...rest } = user._doc;
+      res
+        .cookie("access-token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(rest);
+    }
   } catch (error) {
     next(error);
   }
