fa9850301fa5fa63e8f434eb4fb42b921ee677b8
Himanshu <hv65616@gmail.com>
2023-11-27 14:24:59 +0530

    private route import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import PrivateRoute from "./components/PrivateRoute"
 function App() {
   return <BrowserRouter>
   <Header/>
