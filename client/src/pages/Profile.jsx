dd54ca7caf3980bd4b32b9948ab011a7cdc7300c
Aastha Agarwal <astha1916@gmail.com>
2023-11-27 21:49:23 +0530

    Profile PageUpdation

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,7 +5,7 @@ function Profile() {
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col">
+      <form className="flex flex-col gap-3">
         <img src={currentUser.avatar} alt ="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"></img>
         <input type="text" placeholder="Username" id='username' className="border p-3 rounded-lg" ></input>
         <input type="email" placeholder="Email" id = 'email' className="border p-3 rounded-lg" ></input>
