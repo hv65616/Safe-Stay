1656273a5c65e04b8d5032b77947a45a728df1ef
Aastha Agarwal <astha1916@gmail.com>
2023-11-27 22:01:51 +0530

    Profile Image Upload Functionality Added

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,14 @@
 import { useSelector } from "react-redux"
-
+import { useRef } from "react"
 function Profile() {
+  const fileRef = useRef(null)
   const {currentUser} = useSelector((state) => state.user)
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-3">
-        <img src={currentUser.avatar} alt ="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"></img>
+        <input type="file" ref = "fileRef" hidden accept='image/*'></input>
+        <img onClick={()=>fileRef.current.click()} src={currentUser.avatar} alt ="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"></img>
         <input type="text" placeholder="Username" id='username' className="border p-3 rounded-lg" ></input>
         <input type="email" placeholder="Email" id = 'email' className="border p-3 rounded-lg" ></input>
         <input type="password" placeholder="Password" id = 'password' className="border p-3 rounded-lg" ></input>
