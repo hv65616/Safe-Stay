5d904b20e6170a94d06657c3bed82c45ac3140b9
Himanshu <hv65616@gmail.com>
2023-11-27 14:17:14 +0530

    photo url bug fix

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -65,7 +65,7 @@ export const google = async (req, res, next) => {
           Math.random().toString(36).slice(-8),
         email: req.body.email,
         password: hashedPassword,
-        avatar: req.body.photoURL,
+        avatar: req.body.photo,
       });
       await newuser.save();
       const token = jwt.sign({ id: newuser._id }, process.env.JWTSECRET);
