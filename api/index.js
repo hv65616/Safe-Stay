e3e6aa62d6b4e7e636d9a39f13baf3abc656a37b
Himanshu <hv65616@gmail.com>
2023-11-29 12:04:44 +0530

    cookie parser

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userrouter from "./routes/user.route.js";
 import authrouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 mongoose
   .connect(process.env.MONGOURI)
@@ -14,6 +15,7 @@ mongoose
   });
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/user", userrouter);
 app.use("/api/auth", authrouter);
 app.use((err, req, res, next) => {
