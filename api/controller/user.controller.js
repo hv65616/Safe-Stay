8c8e9e3e4e12893016bcd083fa129837b2949031
Himanshu <hv65616@gmail.com>
2023-11-30 14:52:51 +0530

    delete controller impleteed

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -38,3 +38,16 @@ export const updateuser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteuser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorhandler(401, "You can only delete your own account"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
