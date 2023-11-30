821cccc2154d5db6554427a3a800b94bb0a75825
Himanshu <hv65616@gmail.com>
2023-11-30 20:47:06 +0530

    listing route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userrouter from "./routes/user.route.js";
 import authrouter from "./routes/auth.route.js";
+import listingrouter from "./routes/listing.route.js"
 import cookieParser from "cookie-parser";
 dotenv.config();
 mongoose
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userrouter);
 app.use("/api/auth", authrouter);
+app.use("/api/listing",listingrouter);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
