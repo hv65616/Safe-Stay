06468f6d9b1d2795e35e5f07ade99580fe44e604
Himanshu <hv65616@gmail.com>
2023-11-30 14:52:24 +0530

    delete route added

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { test, updateuser } from "../controller/user.controller.js";
+import { test, updateuser,deleteuser } from "../controller/user.controller.js";
 import { verifyuser } from "../utils/verifyuser.js";
 
 const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyuser, updateuser);
+router.delete("/delete/:id",verifyuser,deleteuser);
 export default router;
