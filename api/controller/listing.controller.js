fef4437fc04df62102d1acab99a0f7f9b3b96d96
Himanshu <hv65616@gmail.com>
2023-11-30 20:48:02 +0530

    listing create controller implemented

---
+++ api/controller/listing.controller.js
@@ -0,0 +1,9 @@
+import Listing from "../models/listing.model.js";
+export const createlisting = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
