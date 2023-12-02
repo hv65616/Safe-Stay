4c6c0354ce056712b4cce177d41dbade59671fbb
Himanshu <hv65616@gmail.com>
2023-12-02 23:28:35 +0530

    user listing route created

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { test, updateuser,deleteuser } from "../controller/user.controller.js";
+import {
+  test,
+  updateuser,
+  deleteuser,
+  getuserlisting,
+} from "../controller/user.controller.js";
 import { verifyuser } from "../utils/verifyuser.js";
 
 const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyuser, updateuser);
 router.delete("/delete/:id",verifyuser,deleteuser);
+router.get("/listings/:id",verifyuser,getuserlisting);
 export default router;
