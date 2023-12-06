5e9130b67de38587d87cca8a41119682ac4fe424
Himanshu <hv65616@gmail.com>
2023-12-06 14:16:32 +0530

    user listing controller implemented

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,6 +1,7 @@
 import { errorhandler } from "../utils/error.js";
 import bcrypt from "bcrypt";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 export const test = (req, res) => {
   res.json({
     success: "Successful",
@@ -49,5 +50,18 @@ export const deleteuser = async (req, res, next) => {
     res.status(200).json("User has been deleted");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getuserlisting = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorhandler(401, "You can only view your own listing"));
   }
 };
