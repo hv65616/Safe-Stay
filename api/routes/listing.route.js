f6d23c502ad94031da7ac7ad60aa6d89917b2880
Himanshu <hv65616@gmail.com>
2023-12-07 11:40:57 +0530

    update listing route added

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router();
-import { createlisting ,deleteListing} from "../controller/listing.controller.js";
+import { createlisting ,deleteListing,updatelisting} from "../controller/listing.controller.js";
 import { verifyuser } from "../utils/verifyuser.js";
 router.post("/create",verifyuser, createlisting);
 router.delete('/delete/:id',verifyuser,deleteListing );
+router.post("/update/:id",verifyuser,updatelisting);
 export default router;
