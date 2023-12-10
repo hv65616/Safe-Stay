0118e134e4da93186d94cb06d8ec10e011f01039
Himanshu <hv65616@gmail.com>
2023-12-10 23:13:14 +0530

    get all listing route added

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,16 @@
 import express from "express";
 const router = express.Router();
-import { createlisting ,deleteListing,updatelisting,getListing} from "../controller/listing.controller.js";
+import {
+  createlisting,
+  deleteListing,
+  updatelisting,
+  getListing,
+  getlistings,
+} from "../controller/listing.controller.js";
 import { verifyuser } from "../utils/verifyuser.js";
-router.post("/create",verifyuser, createlisting);
-router.delete('/delete/:id',verifyuser,deleteListing );
-router.post("/update/:id",verifyuser,updatelisting);
-router.get('/get/:id',getListing);
+router.post("/create", verifyuser, createlisting);
+router.delete("/delete/:id", verifyuser, deleteListing);
+router.post("/update/:id", verifyuser, updatelisting);
+router.get("/get/:id", getListing);
+router.get("/get", getlistings);
 export default router;
