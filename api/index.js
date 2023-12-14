19b79185f3fa9207ec274e3d1aa72670008dcb54
Himanshu <hv65616@gmail.com>
2023-12-14 18:23:35 +0530

    deploy

--- api/index.js
+++ api/index.js
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userrouter from "./routes/user.route.js";
 import authrouter from "./routes/auth.route.js";
-import listingrouter from "./routes/listing.route.js"
+import listingrouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 mongoose
   .connect(process.env.MONGOURI)
@@ -14,12 +15,18 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userrouter);
 app.use("/api/auth", authrouter);
-app.use("/api/listing",listingrouter);
+app.use("/api/listing", listingrouter);
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
